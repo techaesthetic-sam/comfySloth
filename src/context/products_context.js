0f9631e5b5b185635b28d86e42773087510049a7
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-02-22 11:30:07 +0530

    featured Products

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -15,6 +15,10 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -29,9 +33,20 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
 
-  // useEffect(() => {
-  //   openSidebar();
-  // }, []);
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: data });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
