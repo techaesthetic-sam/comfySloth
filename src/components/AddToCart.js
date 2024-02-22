2788ca514b854fa898a66778bd378c46d6420b8e
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-02-22 23:40:35 +0530

    single product completed

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -9,6 +9,22 @@ const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) tempAmount = stock;
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) tempAmount = 1;
+      return tempAmount;
+    });
+  };
   return (
     <Wrapper>
       <div className="colors">
@@ -29,6 +45,13 @@ const AddToCart = ({ product }) => {
             );
           })}
         </div>
+      </div>
+      <div className="btn-container">
+        <AmountButtons
+          increase={increase}
+          decrease={decrease}
+          amount={amount}
+        />
       </div>
     </Wrapper>
   );
