573aa757dc08212a28d0e055d491a8905db404f4
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2024-02-22 16:15:43 +0530

    displayedImages

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const ProductImages = () => {
-  return <h4>product images</h4>
-}
+const ProductImages = ({ images = [{ url: "" }] }) => {
+  const [main, setMain] = useState(images[0]);
+  const handleImageIndex = (index) => {
+    setMain(images[index]);
+  };
+  return (
+    <Wrapper>
+      <img src={main.url} alt="main img" className="main" />
+      <div className="gallery">
+        {images.map((image, index) => {
+          return (
+            <img
+              src={image.url}
+              key={index}
+              alt={image.filename}
+              // onClick={() => setMain(images[index])}
+              onClick={() => handleImageIndex(index)}
+              className={`${image.url === main.url ? "active" : null}`}
+            />
+          );
+        })}
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   .main {
@@ -48,6 +70,6 @@ const Wrapper = styled.section`
       }
     }
   }
-`
+`;
 
-export default ProductImages
+export default ProductImages;
